d7da31a4fb0be9b4c913321ea7b922433d589dfc
Mikhail <mihail.vilms@gmail.com>
2019-10-24 16:50:27 -0500

    Added tests

---
+++ EDU_IComparable_Vs_IComparer_Tests/EmployeeTests.cs
@@ -0,0 +1,57 @@
+﻿using EDU_IComparable_Vs_IComparer.Helpers;
+using EDU_IComparable_Vs_IComparer.Models;
+using EDU_IComparable_Vs_IComparer.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace EDU_IComparable_Vs_IComparer.Tests
+{
+    [TestClass()]
+    public class EmployeeTests
+    {
+        [TestMethod()]
+        public void CompareTo_Test()
+        {
+            List<Employee> employees = EmployeesTestData.GetListOf10Employees();
+
+            employees.Sort();
+
+            Assert.AreEqual("Anthony", employees.First().LastName);
+            Assert.AreEqual("Zimmerman", employees.Last().LastName);
+        }
+
+        [TestMethod()]
+        public void SortBySalary_Test()
+        {
+            List<Employee> employees = EmployeesTestData.GetListOf10Employees();
+
+            employees.Sort(new SortBySalaryHelper());
+
+            foreach (Employee employee in employees)
+            {
+                Trace.WriteLine(employee + "; " + employee.CalculateTotalSalary());
+            }
+
+            Assert.AreEqual("Downs", employees.First().LastName);
+            Assert.AreEqual("Friedman", employees.Last().LastName);
+        }
+
+        [TestMethod()]
+        public void SortByBirthDay_Test()
+        {
+            List<Employee> employees = EmployeesTestData.GetListOf10Employees();
+
+            employees.Sort(new SortByBirthDateHelper());
+
+            foreach (Employee employee in employees)
+            {
+                Trace.WriteLine(employee + "; " + employee.CalculateTotalSalary());
+            }
+
+            Assert.AreEqual("Beard", employees.First().LastName);
+            Assert.AreEqual("Everett", employees.Last().LastName);
+        }
+    }
+}
